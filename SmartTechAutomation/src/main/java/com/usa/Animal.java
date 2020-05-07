cb5e60a42fba0f57611b4138e5b902a04e91c02e
sobha <sobha@LAPTOP-2JKBDL4I.lan1>
2020-05-07 17:37:58 -0400

    by Sobhan

---
+++ SmartTechAutomation/src/main/java/com/usa/Animal.java
@@ -0,0 +1,33 @@
+package com.usa;
+
+public class Animal {
+	
+	
+	public static void main(String[] args) {
+		
+		Animal obj = new Animal();
+		Animal.getMoney();
+		obj.getSalary();
+		
+		System.out.println(" I love java");
+				
+		int a =10;
+		int b =20;
+		double g = 10.20;
+		int c= (a+b);
+		
+		System.out.println(c);
+		
+	}	
+	
+	public void getSalary() {
+		
+		System.out.println("I am learning java");
+	}
+public static void getMoney() {
+		
+		System.out.println("I need my money");
+	}
+	
+
+}
